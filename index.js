5af6b2662834c524f96ce3c032b84b8b3ae9dd41
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2022-11-01 18:38:15 +0530

    new

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function otherName()
-{
-
-
-}
-
 function gitrepo() {
 
-
     var input = document.getElementById("input").value;
-    // alert(input);
     var url = (input + "/archive/refs/heads/main.zip");
+
+    var x = input.slice(0,1);
+    if (x !='h') {
+        url = ("https://"+ input + "/archive/refs/heads/main.zip");
+    }
+    
     var myURL = decodeURI(url);
     window.location=myURL;
 
